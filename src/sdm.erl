d0ce2527045700b05948d8b75e792d077bd33257
Ekimov Artem <ekimov-artem@ya.ru>
2015-08-17 00:30:23 +0300

    Release maps as a KV storage; sync/async get

--- src/sdm.erl
+++ src/sdm.erl
@@ -2,9 +2,11 @@
 
 -bihaviour(gen_server).
 
--export([start/0, start_link/0, get/1, get/2, post/2, delete/1]).
+-export([start/0, start_link/0, post/2, get_sync/1, get_sync/2, get_async/1, get_async/2]).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+-define(DEF_MSG_TTL, 30000).
 
 %%====================================================================
 %% API functions
@@ -16,17 +18,23 @@ start() ->
 start_link() ->
 	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
-get(Key) ->
-	gen_server:call(?MODULE, {get, Key}).
-
-get(Key, ReplyTo) ->
-	gen_server:call(?MODULE, {get, Key, ReplyTo}).
-
 post(Key, Msg) ->
-	gen_server:call(?MODULE, {post, Key, Msg}).
+	post(Key, Msg, ?DEF_MSG_TTL).
 
-delete(Key) ->
-	gen_server:call(?MODULE, {delete, Key}).
+post(Key, Msg, Timeout) ->
+	gen_server:call(?MODULE, {post, Key, Msg, self(), Timeout}, round(1.5 * Timeout)).
+
+get_sync(Key) ->
+	get_sync(Key, ?DEF_MSG_TTL).
+
+get_sync(Key, Timeout) ->
+	gen_server:call(?MODULE, {get_sync, Key, Timeout}, round(1.5 * Timeout)).
+
+get_async(Key) ->
+	get_async(Key, ?DEF_MSG_TTL).
+
+get_async(Key, Timeout) ->
+	gen_server:cast(?MODULE, {get_async, Key, self(), Timeout}).
 
 %%====================================================================
 %% gen_server callbacks
@@ -37,86 +45,96 @@ delete(Key) ->
 %%--------------------------------------------------------------------
 
 init([]) ->
-	% ClearingInterval = application:get_env(sdm, clearing_interval, 60000),
-	MessageLifetime = application:get_env(sdm, message_lifetime, 60),
-	% timer:send_interval(ClearingInterval, clear),
-	{ok, #{list => [], lifetime => MessageLifetime}}.
+	{ok, #{}}.
 
 %%--------------------------------------------------------------------
 %% Description: Handling call messages
 %%--------------------------------------------------------------------
 
-handle_call({get, Key}, Client, #{list := List} = State) ->
-	case lists:keyfind(Key, 1, List) of
-		false ->
-			Timestamp = calendar:datetime_to_gregorian_seconds(calendar:local_time()),
-			{noreply, State#{list => [{Key, [Client], undefined, Timestamp} | List]}};
-		{Key, Waiting, undefined, Timestamp} ->
-			{noreply, State#{list => lists:keyreplace(Key, 1, {Key, [Client | Waiting], undefined, Timestamp})}};
-		{Key, _, Msg, _} ->
-			{reply, {ok, Msg}, State}
+handle_call({get_sync, Key, Timeout}, Client, Messages) ->
+	case maps:get(Key, Messages, undefined) of
+		undefined ->
+			TTL = application:get_env(sdm, msgttl, Timeout),
+			timer:send_after(TTL, {timeout, Key}),
+			{noreply, maps:put(Key, {client, Client}, Messages)};
+		{message, Message, From} ->
+			From ! {sdm, Key, sent},
+			{reply, {ok, Message}, maps:remove(Key, Messages)};
+		_ ->
+			{reply, {error, already_waiting}, Messages}
 	end;
 
-handle_call({get, Key, ReplyTo}, _, #{list := List} = State) ->
-	case lists:keyfind(Key, 1, List) of
-		false ->
-			Timestamp = calendar:datetime_to_gregorian_seconds(calendar:local_time()),
-			{reply, ok, State#{list => [{Key, ReplyTo, undefined, Timestamp} | List]}};
-		{Key, Waiting, undefined, Timestamp} ->
-			{reply, ok, State#{list => lists:keyreplace(Key, 1, {Key, [ReplyTo | Waiting], undefined, Timestamp})}};
-		{Key, _, Msg, _} ->
-			ReplyTo ! {sdm, Key, Msg},
-			{reply, ok, State}
+handle_call({post, Key, Message, From, Timeout}, _, Messages) ->
+	case maps:get(Key, Messages, undefined) of
+		undefined ->
+			timer:send_after(Timeout, {timeout, Key}),
+			{reply, ok, maps:put(Key, {message, Message, From}, Messages)};
+		{client, Client} ->
+			gen_server:reply(Client, {ok, Message}),
+			From ! {sdm, Key, sent},
+			{reply, ok, maps:remove(Key, Messages)};
+		{replyto, ReplyTo} ->
+			ReplyTo ! {sdm, Key, {ok, Message}},
+			From ! {sdm, Key, sent},
+			{reply, ok, maps:remove(Key, Messages)};
+		_ ->
+			{reply, {error, already_posted}, Messages}
 	end;
 
-handle_call({post, Key, Msg}, _, #{list := List} = State) ->
-	case lists:keyfind(Key, 1, List) of
-		false ->
-			Timestamp = calendar:datetime_to_gregorian_seconds(calendar:local_time()),
-			{reply, ok, State#{list => [{Key, [], Msg, Timestamp} | List]}};
-		{Key, [], _, _} ->
-			Timestamp = calendar:datetime_to_gregorian_seconds(calendar:local_time()),
-			{reply, ok, State#{lists => lists:keyreplace(Key, 1, {Key, [], Msg, Timestamp})}};
-		{Key, Waiting, undefined, Timestamp} ->
-			ok = mailing(Waiting, Key, Msg),
-			{reply, ok, State#{list => lists:keyreplace(Key, 1, {Key, [], undefined, Timestamp})}}
-	end;
-
-handle_call({delete, Key}, _, #{list := List} = State) ->
-	{reply, ok, State#{list => list:keydelete(Key, 1, List)}};
-
-handle_call(Request, From, State) ->
-	Error = {error, {?MODULE, ?LINE, {From, Request}}},
-	{stop, Error, Error, State}.
+handle_call(_Request, _From, Messages) ->
+	{reply, {error, not_matched}, Messages}.
 
 %%--------------------------------------------------------------------
 %% Description: Handling cast messages
 %%--------------------------------------------------------------------
 
-handle_cast(Message, State) ->
-	{stop, {error, {?MODULE, ?LINE, Message}}, State}.
+handle_cast({get_async, Key, ReplyTo, Timeout}, Messages) ->
+	case maps:get(Key, Messages, undefined) of
+		undefined ->
+			timer:send_after(Timeout, {remove, Key}),
+			{noreply, maps:put(Key, {replyto, ReplyTo}, Messages)};
+		{message, Message, From} ->
+			ReplyTo ! {sdm, Key, {ok, Message}},
+			From ! {sdm, Key, sent},
+			{noreply, maps:remove(Key, Messages)};
+		_ ->
+			ReplyTo ! {sdm, Key, {error, already_waiting}},
+			{noreply, Messages}
+	end;
+
+handle_cast(_Message, Messages) ->
+	{noreply, Messages}.
 
 %%--------------------------------------------------------------------
 %% Description: Handling all non call/cast messages
 %%--------------------------------------------------------------------
 
-handle_info(clear, #{list := List, lifetime := MessageLifetime} = State) ->
-	TimestampNow = calendar:datetime_to_gregorian_seconds(calendar:local_time()),
-	Fun = fun
-		({_, _, _, Timestamp}, Acc) when TimestampNow - Timestamp > MessageLifetime -> Acc;
-		(Rec, Acc) -> [Rec | Acc]
-	end,
-	{noreply, State#{list => lists:foldl(Fun, [], List)}};
+handle_info({timeout, Key}, Messages) ->
+	case maps:get(Key, Messages, undefined) of
+		undefined ->
+			{noreply, Messages};
+		{message, _Message, From} ->
+			From ! {sdm, Key, timeout},
+			{noreply, maps:remove(Key, Messages)};
+		{client, Client} ->
+			gen_server:reply(Client, {error, timeout}),
+			{noreply, maps:remove(Key, Messages)};
+		{replyto, ReplyTo} ->
+			ReplyTo ! {sdm, Key, {error, timeout}},
+			{noreply, maps:remove(Key, Messages)};
+		_ ->
+			{noreply, Messages}
+	end;
 
-handle_info(Info, State) ->
-	{stop, {error, {?MODULE, ?LINE, Info}}, State}.
+handle_info(_Info, Messages) ->
+	{noreply, Messages}.
 
 %%--------------------------------------------------------------------
 %% Function: terminate(Reason, State) -> void()
 %%--------------------------------------------------------------------
 
-terminate(normal, _) ->	ok;
-terminate(Reason, State) -> lager:error("terminate:~nReason: ~p~nState: ~p", [Reason, State]), ok.
+terminate(_Reason, _Messages) -> 
+	ok.
 
 %%--------------------------------------------------------------------
 %% Func: code_change(OldVsn, State, Extra) -> {ok, NewState}
@@ -128,12 +146,3 @@ code_change(_OldVsn, State, _Extra) ->
 %%--------------------------------------------------------------------
 %%% Internal functions
 %%--------------------------------------------------------------------
-
-mailing([], _, _) ->
-	ok;
-mailing([ReplyTo | Waiting], Key, Msg) when is_pid(ReplyTo) ->
-	ReplyTo ! {sdm, Key, Msg},
-	mailing(Waiting, Key, Msg);
-mailing([Client | Waiting], Key, Msg) when is_tuple(Client) ->
-	gen_server:reply(Client, {ok, Msg}),
-	mailing(Waiting, Key, Msg).
